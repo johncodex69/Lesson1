b6e95725d953ccd5a78cfae39fb87a451889e304
unknown <johnngugi2005@gmail.com>
2023-06-06 06:30:02 +0300

    integers

--- variables.py
+++ variables.py
@@ -1,7 +1,48 @@
-num1 = 2
-num2 = 6
-num3 = 8
+# num1 = 2
+# num2 = 6
+# num3 = 8
 
-result = num1 + num2 + num3
-print( result )
+# result = num1 + num2 + num3
+# print( result )
 
+# print(2 + 6 + 8)
+# print(8 - 2)
+
+# #Multiplication
+# print(3 * 2)
+
+# #Division
+# print(3 / 2)
+
+# #Floor division -cancels decimals
+# print(3 // 2)
+
+# #Exponent -square
+# print(3 ** 2)
+
+# #Modulus -can be used to determine whether number is odd or even
+#           #by dividing by 2
+# print(3 % 2)
+# print(4 % 2)
+
+# #Incrementing
+# num = 2
+
+# num += 1
+# print(num) 
+
+# num *= 10
+# print(num) 
+
+# #Rounding off
+# print(round(4.65))
+# print(round(4.65, 1))
+
+#Casting -converts strings to integers
+num_1 = '100'
+num_2 = '200'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
