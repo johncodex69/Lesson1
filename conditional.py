9a35d05b9c1068096026983b6f8a75a1a1692f6f
unknown <johnngugi2005@gmail.com>
2023-06-07 06:28:15 +0300

    conditionals

--- conditional.py
+++ conditional.py
@@ -1,9 +1,24 @@
-temp = eval(input("input temp:"))
-if temp < 0:
-    print("wrong input")
+# temp = eval(input("input temp:"))
+# if temp < 0:
+#     print("wrong input")
+# else:
+#     if temp > 50:
+#         x = temp/2
+#         print(x)
+#     else:
+#         print("number is less than 50")
+
+#Boolean operations
+user = "Admin"
+logged_in = False
+
+# #You can replace and with or
+# if user == "Admin" and logged_in:
+#     print("Admin page")
+# else:
+#     print("wrong credentials")
+
+if not logged_in:
+    print("Please log_in")
 else:
-    if temp > 50:
-        x = temp/2
-        print(x)
-    else:
-        print("number is less than 50")
+    print("Welcome")
--- integers.py
+++ integers.py
@@ -38,11 +38,11 @@
 # print(round(4.65))
 # print(round(4.65, 1))
 
-#Casting -converts strings to integers
-num_1 = '100'
-num_2 = '200'
+# #Casting -converts strings to integers
+# num_1 = '100'
+# num_2 = '200'
 
-num_1 = int(num_1)
-num_2 = int(num_2)
+# num_1 = int(num_1)
+# num_2 = int(num_2)
 
-print(num_1 + num_2)
+# print(num_1 + num_2)
